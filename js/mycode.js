bf633416534f3f1239c17d853b4b7f0582c98096
MattiaCiolli <mattia93c@gmail.com>
2016-08-18 12:45:18 +0200

    totale, pulizia e slick base

--- js/mycode.js
+++ js/mycode.js
@@ -14,8 +14,16 @@ function syncToServer( value, op ) {
 		success:  function(jsonData) {
 					$('.up').remove();
 					var listItem;
+					var totalPrice=0;
+
 					for ( var ob in jsonData ) {
-						listItem = '<div data-id="' + jsonData[ob].id + '" id="' + jsonData[ob].id + '" class=\"col-lg-4 up\"><h2>' + jsonData[ob].objectname + '</h2><p class=\"text-danger\">'+jsonData[ob].description+'</p><p></br><h4>Quantity: <b>'+jsonData[ob].quantity+'</b></h4><p></br><h4>Total price: <b>'+jsonData[ob].price+'€</b></h4></br></p></p><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-default editbtn\" data-toggle=\"modal\" data-target=\"#editModal\">Edit <span class="glyphicon glyphicon-edit"></span></a><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-primary btn-danger confirm-delete\" data-toggle=\"modal\" data-target=\"#basicModal\">Remove <span class="glyphicon glyphicon-trash"></span></a></div>';
+						totalPrice=totalPrice+(jsonData[ob].price*jsonData[ob].quantity);
+					}
+					var showprices='<div class="alert alert-info up" role="alert"><h3><b>Total price: '+totalPrice+'€</b></h3></div>';
+					$(showprices).appendTo('#a');
+
+					for ( var ob in jsonData ) {
+						listItem = '<div data-id="' + jsonData[ob].id + '" id="' + jsonData[ob].id + '" class=\"col-lg-4 up\"><h2>' + jsonData[ob].objectname + '</h2><p class=\"text-danger\">'+jsonData[ob].description+'</p><p></br><h4>Quantity: <b>'+jsonData[ob].quantity+'</b></h4><p></br><h4>Unit price: <b>'+jsonData[ob].price+'€</b></h4></br><h4>Total price: <b>'+jsonData[ob].price*jsonData[ob].quantity+'€</b></h4></br></p></p><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-default editbtn\" data-toggle=\"modal\" data-target=\"#editModal\">Edit <span class="glyphicon glyphicon-edit"></span></a><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-primary btn-danger confirm-delete\" data-toggle=\"modal\" data-target=\"#basicModal\">Remove <span class="glyphicon glyphicon-trash"></span></a></div>';
 						$(listItem).appendTo('#a');
 					};
 				},
@@ -54,8 +62,16 @@ $(function() {
 					$("#loading").hide();
 
 					var listItem;
+					var totalPrice=0;
+
 					for ( var ob in jsonData ) {
-						listItem = '<div data-id="' + jsonData[ob].id + '" id="' + jsonData[ob].id + '" class=\"col-lg-4 up\"><h2>' + jsonData[ob].objectname + '</h2><p class=\"text-danger\">'+jsonData[ob].description+'</p><p></br><h4>Quantity: <b>'+jsonData[ob].quantity+'</b></h4><p></br><h4>Total price: <b>'+jsonData[ob].price+'€</b></h4></br></p></p><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-default editbtn\" data-toggle=\"modal\" data-target=\"#editModal\">Edit <span class="glyphicon glyphicon-edit"></span></a><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-primary btn-danger confirm-delete\" data-toggle=\"modal\" data-target=\"#basicModal\">Remove <span class="glyphicon glyphicon-trash"></span></a></div>';
+						totalPrice=totalPrice+(jsonData[ob].price*jsonData[ob].quantity);
+					}
+					var showprices='<div class="alert alert-info up" role="alert"><h3><b>Total price: '+totalPrice+'€</b></h3></div>';
+					$(showprices).appendTo('#a');
+
+					for ( var ob in jsonData ) {
+						listItem = '<div data-id="' + jsonData[ob].id + '" id="' + jsonData[ob].id + '" class=\"col-lg-4 up\"><h2>' + jsonData[ob].objectname + '</h2><p class=\"text-danger\">'+jsonData[ob].description+'</p><p></br><h4>Quantity: <b>'+jsonData[ob].quantity+'</b></h4><p></br><h4>Unit price: <b>'+jsonData[ob].price+'€</b></h4></br><h4>Total price: <b>'+jsonData[ob].price*jsonData[ob].quantity+'€</b></h4></br></p></p><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-default editbtn\" data-toggle=\"modal\" data-target=\"#editModal\">Edit <span class="glyphicon glyphicon-edit"></span></a><a data-id="' + jsonData[ob].id + '" href=\"#\" class=\"btn btn-primary btn-danger confirm-delete\" data-toggle=\"modal\" data-target=\"#basicModal\">Remove <span class="glyphicon glyphicon-trash"></span></a></div>';
 						$(listItem).appendTo('#a');
 					}
 
@@ -238,6 +254,16 @@ $('#btnDel').click(function() {
   	$('[data-id='+deletedId+']').remove();
   	syncToServer( deletedId, "delTask");
   	$('#basicModal').modal('hide');
+});
+
+
+$(document).ready(function(){
+  $('.autoplay').slick({
+    slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  });
 });
 
 
